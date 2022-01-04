7fb3b1282b6dd9404a88797bf971247381b19d74
kimjongsoon <goy0506@naver.com>
2022-01-04 12:45:44 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,29 @@
+ // 2. This code loads the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
+ tag.src = "https://www.youtube.com/iframe_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// onYouTubePlayerAPIReady 함수 이름은,
+// Youtube IFrame Player API에서 사용하는 이름이기 때문에,
+// 다르게 지정하면 동작하지 않습니다!
+// 그리고 함수는 전역(Global) 등록해야 합니다!
+
+ function onYouTubeIframeAPIReady() {
+     //<div id="player"></div>
+   new YT.Player('player', {
+     videoId: 'An6LvWQuj_8', //https://www.youtube.com/watch?v=An6LvWQuj_8 에서 아이디값
+
+     //제어
+     playerVars: {
+         autoplay:true, //자동 재생 여부
+         loop: true, //반복 재생 여부
+         playlist:'An6LvWQuj_8' //반복 재생할 유튜브 영상 ID목록
+     },
+     events: {
+         onReady: function(event){
+             event.target.mute() //음소거
+         }
+     }
+   });
+ }
